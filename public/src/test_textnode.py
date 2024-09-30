2b3bd6827099aa6db07b94e93d8421cdb272a7a4
gleni1 <glenipoleshi@gmail.com>
2024-09-30 01:12:49 -0400

    Inline and block markdown
    Finished up the features for the inline and block markdown conversion

--- public/src/test_textnode.py
+++ public/src/test_textnode.py
@@ -8,7 +8,7 @@ from textnode import(
     text_type_code,
     text_type_image,
     text_type_link,
-
+    text_node_to_html_node
 )
 
 class TestTextNode(unittest.TestCase):
@@ -41,6 +41,56 @@ class TestTextNode(unittest.TestCase):
         )
 
 
+class TestTextNodeToHTMLNode(unittest.TestCase):
+    def test_text(self):
+        node = TextNode("This is a text node", text_type_text)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_image(self):
+        node = TextNode("This is an image", text_type_image, "https://www.boot.dev")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(
+            html_node.props,
+            {"src": "https://www.boot.dev", "alt": "This is an image"},
+        )
+    
+    def test_bold(self):
+        node = TextNode("This is bold", text_type_bold)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This is bold")
+
+
+
 
 if __name__ == "__main__":
     unittest.main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
